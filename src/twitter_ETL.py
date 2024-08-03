383c052a4866bba96df3c3b4a58b26229a2c8459
Hamza Ali <mac@Hamza-MacBook-Pro.local>
2024-08-04 00:35:33 +0500

    EC2 configured

--- src/twitter_ETL.py
+++ src/twitter_ETL.py
@@ -6,9 +6,41 @@ import s3fs
 from dotenv import load_dotenv
 import os
 
-load_dotenv("./Twitter-Pipeline-/.env")
+load_dotenv('/Users/mac/Desktop/Hamza Ali/Twitter-Pipeline-/.env')
 
-access_key = os.getenv()
-access_secret = os.getenv()
-consumer_key = os.getenv()
-consumer_secret = os.getenv()
+access_key = os.getenv("API_KEY")
+access_secret = os.getenv("API_KEY_SECRET")
+bearer_token = os.getenv("BEARER_TOKEN")
+consumer_key = os.getenv("ACCESS_TOKEN")
+consumer_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+def run_twitter_etl():
+    # Authentication of the twitter:
+    auth = tweepy.OAuthHandler(access_key, access_secret)   
+    auth.set_access_token(consumer_key, consumer_secret) 
+
+    api = tweepy.API(auth)
+    tweets = api.user_timeline(screen_name='@babarazam258', 
+                            count=200,
+                            include_rts = False,
+                            tweet_mode = 'extended')
+
+    tweets_list = []
+    for tweet in tweets:
+        text = tweet._json["full_text"]
+
+        refined_tweet = {"user": tweet.user.screen_name,
+                        'text' : text,
+                        'favorite_count' : tweet.favorite_count,
+                        'retweet_count' : tweet.retweet_count,
+                        'created_at' : tweet.created_at}
+        
+        tweets_list.append(refined_tweet)
+
+    print(tweets_list)
+
+
+run_twitter_etl()
+
+    # df = pd.DataFrame(list)
+    # df.to_csv('refined_tweets.csv')
